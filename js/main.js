1266d7d46fb4c5b1860ecc5bdbccbe9c38700179
pro100Sardor <sardorbek.juraqulov@gmail.com>
2020-10-06 19:10:14 +0500

    WIP, version 1 of the header is ready
    the header is designed to fit all desired widths

---
+++ js/main.js
@@ -0,0 +1,7 @@
+function openHeaderMenyu() {
+  document.getElementById("headerMenyu").style.width = "255px";
+}
+
+function closeHeaderMenyu() {
+  document.getElementById("headerMenyu").style.width = "0";
+}
